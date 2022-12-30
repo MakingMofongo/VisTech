474207257f039f887b3f2c60ccd7f7b099648807
clumsyninja3086 <rasheed3086@gmail.com>
2022-12-30 20:34:27 +0530

    Threading done ez

--- BingChilling/theChilling.py
+++ BingChilling/theChilling.py
@@ -3,34 +3,30 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
+import time
 
 from SpeechIO import stt,tts
 from FaceSave import Capture
+
+import threading
+import queue
+
+# import multiprocessing as mp
+# from multiprocessing import Process
 
 
 
 from PIL import ImageGrab
 
-Capture()
 path = 'BingChilling/Faces/Captured'
 images = []
 classNames = [] #names of captured images without extension
 myList = os.listdir(path)
-print(myList)
-n=-1
-for cl in myList:
-    n+=1
-    curImg = cv2.imread(f'{path}/{cl}')
-    if curImg.all:
-        images.append(curImg)
-        classNames.append(os.path.splitext(cl)[0])
-        print(classNames[n])
-    else:
-        print(f'Couldnt read {cl}')
 
 
 def findEncodings(images):
     encodeList = []
+
     n=-1 #counter used to find name of image if needed
     for img in images:
         n+=1
@@ -50,44 +46,83 @@ def findEncodings(images):
     else:
         print('couldnt find a single face, exiting....')
         exit()
- 
- 
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
- 
-encodeListKnown = findEncodings(images)
-print('Encoding Complete')
- 
-cap = cv2.VideoCapture(0)
- 
-while True:
-    success, img = cap.read()
-    #img = captureScreen()
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
 
 
-    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+def snap(cap):
+    while True:
+        global img
+        success, img = cap.read()
+        global imgS
+        imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+def liveEncodings():
+    while True:
+
+        global facesCurFrame
+        facesCurFrame= face_recognition.face_locations(imgS)
         
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        #print(faceDis)
-        matchIndex = np.argmin(faceDis)
-    
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
-            #print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+        global encodesCurFrame
+        encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
 
-    cv2.imshow('Webcam',img)
-    cv2.waitKey(1)
- 
+    
+def main():
+
+    Capture()
+
+
+    print(myList)
+    n=-1
+    for cl in myList:
+        n+=1
+        curImg = cv2.imread(f'{path}/{cl}')
+        if curImg.all:
+            images.append(curImg)
+            classNames.append(os.path.splitext(cl)[0])
+            print(classNames[n])
+        else:
+            print(f'Couldnt read {cl}') 
+    encodeListKnown = findEncodings(images)
+    print('Encoding Complete')
+    
+    cap = cv2.VideoCapture(0)
+
+    thread = threading.Thread(target=liveEncodings)
+    thread2 = threading.Thread(target=snap,args=(cap,))
+    thread2.start()
+    time.sleep(1)
+    thread.start()
+    time.sleep(1)
+    while True:
+
+
+        # if ((t2-t1)* 10**3) >0:
+        #     print(f'processing done in: {(t2-t1)* 10**3}ms')
+        #     print('done')
+
+
+
+        # t1=time.time()
+        # for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+            
+        #     matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        #     faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        #     #print(faceDis)
+        #     matchIndex = np.argmin(faceDis)
+        
+        #     if matches[matchIndex]:
+        #         name = classNames[matchIndex].upper()
+        #         #print(name)
+        #         y1,x2,y2,x1 = faceLoc
+        #         y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+        #         cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+        #         cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+        #         cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+        # t2=time.time()
+        # print(f'time for comparing = {(t2-t1)* 10**3}ms')
+        cv2.imshow('Webcam',img)
+        cv2.waitKey(1)
+
+if __name__=='__main__':
+ main()
+    
