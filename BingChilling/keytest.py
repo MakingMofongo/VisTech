3025f20faf2d8e6ab4717134f51c64e389eac4f8
ClumsyNinja3086 <rasheed3086@gmail.com>
2023-04-11 01:33:06 +0530

    From CFI

--- BingChilling/keytest.py
+++
@@ -1,6 +0,0 @@
-import keyboard
-
-while True:
-    keyboard.on_press_key(' ','bruh')
-    break()
-print('bbrrr')
---
+++ import bot_test_gpt.py
@@ -0,0 +1,3 @@
+import bot_test_gpt
+bot_test_gpt.bot_initialize()
+bot_test_gpt.description(bot_test_gpt.bot, 'hi')
